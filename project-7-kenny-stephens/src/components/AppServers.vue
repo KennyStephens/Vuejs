eae3541c0a0824cdcb3152555e8f02d77c44822d
Kenny Stephens <kennethcstephens@gmail.com>
2018-10-02 13:34:26 -0600

    Slight changes.

--- project-7-kenny-stephens/src/components/AppServers.vue
+++ project-7-kenny-stephens/src/components/AppServers.vue
@@ -2,7 +2,7 @@
     <v-list class="pa-3">
         <v-list-tile
             v-for="(index, i) in 5" :key="i">
-            <v-icon color="green" class="mr-1">star</v-icon>
+            <v-icon color="amber accent-2" class="mr-1">computer</v-icon>
             Server #{{ index }}
         </v-list-tile>
     </v-list>
