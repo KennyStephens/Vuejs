a2baef0f221775e46e591735b6e5d940ca2e4bd3
Kenny Stephens <kennethcstephens@gmail.com>
2018-10-02 15:22:23 -0600

    Removed assignment

--- project-7-kenny-stephens/src/components/AppFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <v-footer dark fixed class="pa-2">
-          <h3>All Servers are managed here.</h3>
-    </v-footer>
-</template>
--- project-7-kenny-stephens/src/components/AppServerDetails.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="pa-3">
-        <p>Server Details are currently not updated</p>
-    </div>
-</template>
