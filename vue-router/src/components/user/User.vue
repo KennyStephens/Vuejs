84bed31b7758a3dd26671c89b1dfc811dab5022c
Kenny Stephens <10468762@uvu.edu>
2018-11-05 08:18:30 -0700

    Started working with vue router

---
+++ vue-router/src/components/user/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }}</p>
+        <button class="btn btn-primary" @click="navigateToHome">Go To Home</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
+        methods: {
+            navigateToHome() {
+                this.$router.push('/');
+            }
+        }
+    }
+</script>
