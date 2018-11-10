49b8db5efaa9062e4240bc403d8d3286d78218df
Kenny Stephens <kennethcstephens@gmail.com>
2018-11-10 14:16:15 -0700

    added ability to use rest and spread operators, used mapGetters ,etc

---
+++ vue-x/src/components/AnotherResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Counter is: {{ doublcCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
+
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+      computed: {
+        ...mapGetters([
+          'doubleCounter',
+          'stringCounter'
+          ])
+      }  
+    }
+</script>
